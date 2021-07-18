aa386d7d3f13d2a334fde7583dca7cb550a8f90b
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-07-19 02:21:53 +0500

    Working on creating and deleting followers :rocket:

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import get_user, get_all_users
+from .views import get_user, get_all_users, create_follower, delete_follower
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('', get_user),
     path('all/', get_all_users),
+    path('follower/create', create_follower),
+    path('follower/delete', delete_follower)
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
-from .models import User, Student
-from .serializer import UserSerializer, StudentSerializer, Users
+from .models import User, Student, Follower
+from .serializer import UserSerializer, StudentSerializer, Users, FollowerSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
@@ -30,6 +30,60 @@ def get_all_users(request):
         user = User.objects.all()
         user_serializer = Users(user, many=True)
         return Response(data=user_serializer.data, status=status.HTTP_200_OK)
+    except:
+        response['error'] = "Error occured"
+        return Response(data=response, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_follower(request):
+    response = {}
+    try:
+        user = User.objects.get(email=request.user)  # get authenticated user
+        data = {
+            "follower_id": user.id,
+            "followed_id": request.data['id']
+        }
+        follower_serializer = FollowerSerializer(data=data)
+        if follower_serializer.is_valid():
+            follower_serializer.save()
+            response["success"] = "Follower Added"
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        else:
+            response['error'] = "Error occured"
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        response['error'] = "Error occured"
+        return Response(data=response, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def delete_follower(request):
+    response = {}
+    try:
+        user = User.objects.get(email=request.user)  # get authenticated user
+        followed_id = Follower.objects.get(followed_id=request.data['id'])
+        print("Followed id is")
+        print(followed_id)
+        if followed_id:
+
+            data = {
+                "follower_id": user.id,
+                "followed_id": followed_id
+            }
+            follower_serializer = FollowerSerializer(data=data)
+            if follower_serializer.is_valid():
+                # follower_serializer.save()
+                response["success"] = "Follower Removed"
+                return Response(data=response, status=status.HTTP_201_CREATED)
+            else:
+                print(follower_serializer.errors)
+                response['error'] = "Error occured"
+            return Response(data=response, status=status.HTTP_200_OK)
+        response['error'] = "Invalid Request"
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
     except:
         response['error'] = "Error occured"
         return Response(data=response, status=status.HTTP_404_NOT_FOUND)
