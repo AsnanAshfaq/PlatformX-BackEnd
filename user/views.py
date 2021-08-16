0d8ec6b9ebe63e87daa4ca2ce4687a701d9c031f
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-08-16 20:27:54 +0500

    Sign in and sign up functionality

--- user/views.py
+++ user/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 
 # Create your views here
@@ -23,7 +25,7 @@ def get_user(request):
 
 
 @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def get_all_users(request):
     response = {}
     try:
@@ -34,3 +36,46 @@ def get_all_users(request):
         response['error'] = "Error occured"
         return Response(data=response, status=status.HTTP_404_NOT_FOUND)
 
+
+@api_view(['POST'])
+def create_user(request):
+    response = {}
+    try:
+        # check if username already exist
+        username = User.objects.filter(username=request.data['username'])
+        if username:
+            response['error'] = "User name already exists"
+            print(response)
+            return Response(data=response, status=status.HTTP_200_OK)
+
+        # check if email already exist
+        email = User.objects.filter(email=request.data['email'])
+        if email:
+            response['error'] = "Email already exists"
+            print(response)
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+        # create user with given credentials
+        user = User.objects.create_user(username=request.data['username'], email=request.data['email'],
+                                        password=request.data['password'], first_name=request.data['first_name'],
+                                        last_name=request.data['last_name'])
+        if user:
+            response['success'] = "Your account has been created successfully"
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+        response['error'] = "An error has occurred while creating your account"
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+    except:
+        response['error'] = "An error has occurred while creating your account"
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def get_user_token(request):
+    # print(request.data)
+    print(request.data['email'], request.data['password'])
+    users = User.objects.all()
+    print(users)
+
+    return Response(data="User has requested for token", status=status.HTTP_200_OK)
