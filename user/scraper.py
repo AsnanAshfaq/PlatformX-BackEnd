efa4af1ce0a9f646129d701a447a57d08894fcf7
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-11-29 19:45:59 +0500

    Added web scraper :tada:

--- user/scraper.py
+++ user/scraper.py
@@ -2,11 +2,49 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
+artificial_urls_list = [
+    {
+        "link": "https://www.brookings.edu/research/how-artificial-intelligence-is-transforming-the-world/",
+    },
+
+]
+
+url = "https://www.brookings.edu/research/how-artificial-intelligence-is-transforming-the-world/"
+
+
+# "https://builtin.com/artificial-intelligence",
+#     "https://futureoflife.org/background/benefits-risks-of-artificial-intelligence/?cn-reloaded=1",
+#     "https://www.brookings.edu/research/how-artificial-intelligence-is-transforming-the-world/",
+#     "https://wsimag.com/science-and-technology/64215-artificial-intelligence-has-changed-our-world"
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def scrape_articles(request):
-    response = {}
+    response = []
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    return Response(data=response, status=status.HTTP_200_OK)
+    try:
+        for url in artificial_urls_list:
+            driver.get(url["link"])
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            title = soup.find(name="h1", class_="report-title").string
+            content = soup.find(name="div", class_="summary-text").p.string
+            image = soup.source['srcset']
+            data = {
+                "id": 1,
+                "title": title,
+                "content": content,
+                "image": image,
+                "url": url["link"]
+            }
+            response.append(data)
+
+        return Response(data={"success": "Articles have been scraped", "data": response}, status=status.HTTP_200_OK)
+
+    except:
+        return Response(data={"error": "Error while scraping articles"}, status=status.HTTP_400_BAD_REQUEST)
